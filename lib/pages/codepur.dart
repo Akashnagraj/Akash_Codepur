11a521d78a50196b5c0fa89a2d0a30beb48b5741
Akashnagraj <akashnagraj77@gmail.com>
2024-09-02 12:26:15 +0530

    something completed

--- lib/pages/codepur.dart
+++ lib/pages/codepur.dart
@@ -12,7 +12,11 @@ class _CodePurFlutterState extends State<CodePurFlutter> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 5, 87, 7),
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text("V I S W A B H I S H E K"),
+      ),
+      body: Container(),
+      drawer: Drawer(),
     );
   }
 }
